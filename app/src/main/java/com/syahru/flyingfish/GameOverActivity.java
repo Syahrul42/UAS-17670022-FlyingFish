b9bbe58a9579b0d889b38a68d4e8cd94f0574e6e
Syahrul42 <syahrulsoedjatwa42@gmail.com>
2020-07-08 20:38:48 +0700

    UAS

---
+++ app/src/main/java/com/syahru/flyingfish/GameOverActivity.java
@@ -0,0 +1,39 @@
+package com.syahru.flyingfish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOverActivity extends AppCompatActivity
+{
+    private Button StarGameAgain;
+    private TextView GameScore;
+    private String score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        score = getIntent().getExtras().get("score").toString();
+
+        StarGameAgain = (Button) findViewById(R.id.play_again);
+
+        GameScore = (TextView) findViewById(R.id.gameScore);
+
+        StarGameAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent mainIntent = new Intent(GameOverActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        GameScore.setText("Score " + score);
+    }
+}
